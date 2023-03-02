b2a835281540dd8e48f49c3bc7d42f40166cb9a9
stoicism-sympathei <mogesanonymous@gmail.com>
2023-03-02 06:43:04 +0300

    header

--- binary_trees.h
+++ binary_trees.h
@@ -30,6 +30,21 @@ typedef struct binary_tree_s heap_t;
 
 void binary_tree_print(const binary_tree_t *tree);
 
+/**
+ * struct levelorder_queue_s - Level order traversal queue.
+ * @node: A node of a binary tree.
+ * @next: The next node to traverse to in the binary tree.
+ */
+typedef struct levelorder_queue_s
+{
+	binary_tree_t *node;
+	struct levelorder_queue_s *next;
+} levelorder_queue_t;
+
+/* Printing helper function */
+void binary_tree_print(const binary_tree_t *);
+
+
 /* MANDATORY */
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
